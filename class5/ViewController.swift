24fffbf3b05fd84ab384f45e079500ee1ef19bd6
ravali <burugu.ravali@gmail.com>
2021-10-18 21:02:26 +0100

    Initial Commit

---
+++ class5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  class5
+//
+//  Created by RAVALI Burugu on 18/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
